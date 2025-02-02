e1cb7bb2bc99d44c9b659bb29f68153ee6279105
shin-jisong <86762272+shin-jisong@users.noreply.github.com>
2025-02-02 22:37:41 +0900

    feat: AnnotationHandlerMapping 구현

--- mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMapping.java
+++ mvc/src/main/java/com/interface21/webmvc/servlet/mvc/tobe/AnnotationHandlerMapping.java
@@ -1,11 +1,18 @@
 package com.interface21.webmvc.servlet.mvc.tobe;
 
+import com.interface21.context.stereotype.Controller;
+import com.interface21.web.bind.annotation.RequestMapping;
+import com.interface21.web.bind.annotation.RequestMethod;
 import jakarta.servlet.http.HttpServletRequest;
+import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.lang.reflect.Method;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 public class AnnotationHandlerMapping {
 
@@ -20,10 +27,36 @@ public class AnnotationHandlerMapping {
     }
 
     public void initialize() {
+        Reflections reflections = new Reflections(basePackage);
+        Set<Class<?>> classes = reflections.getTypesAnnotatedWith(Controller.class);
+        for (Class<?> clazz : classes) {
+            initializeController(clazz);
+        }
         log.info("Initialized AnnotationHandlerMapping!");
     }
 
-    public Object getHandler(final HttpServletRequest request) {
-        return null;
+    private void initializeController(Class<?> clazz) {
+        try {
+            Object controller = clazz.getDeclaredConstructor().newInstance();
+            Arrays.stream(clazz.getMethods())
+                    .filter(method -> method.isAnnotationPresent(RequestMapping.class))
+                    .forEach(method -> initializeMethod(method, controller));
+        } catch (Exception e) {
+            log.error("Failed to instantiate controller: {}", clazz.getName(), e);
+        }
+    }
+
+    private void initializeMethod(Method method, Object controller) {
+        RequestMapping requestMapping = method.getAnnotation(RequestMapping.class);
+        HandlerKey key = new HandlerKey(requestMapping.value(), requestMapping.method()[0]);
+        handlerExecutions.put(key, new HandlerExecution(controller, method));
+    }
+
+
+    public HandlerExecution getHandler(final HttpServletRequest request) {
+        String requestUri = request.getRequestURI();
+        RequestMethod requestMethod = RequestMethod.valueOf(request.getMethod());
+        HandlerKey key = new HandlerKey(requestUri, requestMethod);
+        return handlerExecutions.get(key);
     }
 }
